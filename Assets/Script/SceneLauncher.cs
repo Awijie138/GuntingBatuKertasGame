4ca74bdfdba5ac83072debf5facc383314c8b59d
Mutawally Syarawy <mutawally.syarawy@agate.id>
2022-06-21 14:29:28 +0800

    First commit

---
+++ Assets/Script/SceneLauncher.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class SceneLauncher : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+
